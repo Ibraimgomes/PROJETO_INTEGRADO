8c550274b444c246164eda79056a9351e6f1a31e
Ibraim Gomes <ibraimgomes@MacBook-Air-de-Ibraim.local>
2025-04-12 00:13:27 -0300

    totalconectpage 2.0

--- src/components/BarraNavegacao.tsx
+++ src/components/BarraNavegacao.tsx
@@ -15,7 +15,7 @@ export default function BarraNavegacao() {
       {/* Menu desktop */}
       <ul className="hidden md:flex gap-6 text-gray-700">
         <li><Link href="/">CameTáOn</Link></li>
-        <li><Link href="/totalconect">TotalConect</Link></li>
+        <li><Link href="/total_conect">TotalConect</Link></li>
         <li><Link href="#planos">Planos</Link></li>
         <li><Link href="#contato">Contato</Link></li>
         <li><Link href="/login-cliente">Para Empresas</Link></li>
@@ -32,7 +32,7 @@ export default function BarraNavegacao() {
       {menuAberto && (
         <ul className="absolute top-full left-0 w-full bg-white shadow-md flex flex-col items-start gap-4 p-4 text-gray-700 z-50 md:hidden">
           <li><Link href="/" onClick={() => setMenuAberto(false)}>CameTáOn</Link></li>
-          <li><Link href="/totalconect" onClick={() => setMenuAberto(false)}>TotalConect</Link></li>
+          <li><Link href="/tota_lconect" onClick={() => setMenuAberto(false)}>TotalConect</Link></li>
           <li><Link href="#planos" onClick={() => setMenuAberto(false)}>Planos</Link></li>
           <li><Link href="#contato" onClick={() => setMenuAberto(false)}>Contato</Link></li>
           <li><Link href="/login-cliente">Cliente</Link></li>
