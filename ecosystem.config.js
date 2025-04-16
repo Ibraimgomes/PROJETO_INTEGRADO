4758196cfb211e3ded01c2f55a20a458b156d62b
Ibraim Gomes <ibraimgomes@MacBook-Air-de-Ibraim.local>
2025-04-16 01:55:18 -0300

    v10.1

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,7 +5,12 @@ module.exports = {
       script: "npm",
       args: "start",
       env: {
-        NODE_ENV: "production"
+        NODE_ENV: "development",
+        PORT: 3000
+      },
+      env_production: {
+        NODE_ENV: "production",
+        PORT: 3000
       }
     }
   ]
