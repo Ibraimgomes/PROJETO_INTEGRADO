b5b6e5af5e56421cf8009f2fc099fe1fdfd22ec6
Ibraim Gomes <ibraimgomes@MacBook-Air-de-Ibraim.local>
2025-04-25 12:13:59 -0300

    layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,11 @@
+'use client'
+
 import './globals.css'
 import { Geist, Geist_Mono } from 'next/font/google'
 import { Providers } from './providers'
 import BarraNavegacao from '@/components/BarraNavegacao'
-import 'leaflet/dist/leaflet.css';
+import 'leaflet/dist/leaflet.css'
+import { useEffect } from 'react'
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -14,12 +17,11 @@ const geistMono = Geist_Mono({
   subsets: ['latin'],
 })
 
-export const metadata = {
-  title: 'TotalConect Painel',
-  description: 'Administração de lojas locais',
-}
-
 export default function RootLayout({ children }: { children: React.ReactNode }) {
+  useEffect(() => {
+    document.title = 'TotalConect Painel'
+  }, [])
+
   return (
     <html lang="pt-br">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
@@ -31,9 +33,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
 
           {/* Rodapé padrão */}
           <footer className="border-t mt-10 py-6 text-center text-sm text-gray-600">
-            <div className="flex justify-center gap-6">
-
-            </div>
+            <div className="flex justify-center gap-6"></div>
           </footer>
         </Providers>
       </body>
